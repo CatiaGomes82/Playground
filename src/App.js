e5b614b00d2a7faef1c654165b90fde8e5fabf64
CatiaGomes <cgo@codehousegroup.com>
2019-11-08 10:44:23 +0000

    fix button and layout issue

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const customStyles = {
     bottom: '0',
     borderRadius: '15px 15px 0 0',
     padding: 0,
-    maxHeight: 'calc(100vh - 40px)'
+    maxHeight: 'calc(100vh - 70px)'
   }
 };
 
@@ -72,9 +72,11 @@ class App extends React.Component {
               </div>
               <button>Login</button>
             </form>
-            <button className="close-btn" onClick={this.closeModal}>
-              <svg className="close-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 47.971 47.971"><path d="M28.228 23.986L47.092 5.122a2.998 2.998 0 000-4.242 2.998 2.998 0 00-4.242 0L23.986 19.744 5.121.88a2.998 2.998 0 00-4.242 0 2.998 2.998 0 000 4.242l18.865 18.864L.879 42.85a2.998 2.998 0 104.242 4.241l18.865-18.864L42.85 47.091c.586.586 1.354.879 2.121.879s1.535-.293 2.121-.879a2.998 2.998 0 000-4.242L28.228 23.986z"/></svg>
-            </button>
+            <div className="btn-wrap">
+              <button className="close-btn" onClick={this.closeModal}>
+                <svg className="close-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 47.971 47.971"><path d="M28.228 23.986L47.092 5.122a2.998 2.998 0 000-4.242 2.998 2.998 0 00-4.242 0L23.986 19.744 5.121.88a2.998 2.998 0 00-4.242 0 2.998 2.998 0 000 4.242l18.865 18.864L.879 42.85a2.998 2.998 0 104.242 4.241l18.865-18.864L42.85 47.091c.586.586 1.354.879 2.121.879s1.535-.293 2.121-.879a2.998 2.998 0 000-4.242L28.228 23.986z"/></svg>
+              </button>
+            </div>
           </div>
           <div className="promo">
             <div className="promo-text">
